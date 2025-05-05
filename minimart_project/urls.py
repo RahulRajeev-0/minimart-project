4c9e106e456b10449c2776c256302c040e113562
Rahul Rajeev <rahulrajeev836@gmail.com>
2025-05-05 21:42:40 +0530

    tried to add ui to base url

--- minimart_project/urls.py
+++ minimart_project/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import render
+
+
+
 
 
 urlpatterns = [
+     path('', lambda request: render(request, 'home.html'), name='home'), 
     path('admin/', admin.site.urls),
     path('customer/', include('customer.urls')),
     path('product/', include('product.urls')),
